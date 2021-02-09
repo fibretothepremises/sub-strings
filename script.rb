0fa8790715a06bbcd3cc3aeb35f152b54c52029a
Fibre P <fttx5525@gmail.com>
2021-02-09 13:48:37 +1100

    Initial commit

---
+++ script.rb
@@ -0,0 +1,30 @@
+# Implement a method #substrings that takes a word as the first argument and then an array of valid substrings (your dictionary) as the second argument. It should return a hash listing each substring (case insensitive) that was found in the original string and how many times it was found.
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+#   => ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+#   > substrings("below", dictionary)
+#   => { "below" => 1, "low" => 1 }
+# Next, make sure your method can handle multiple words:
+
+#   > substrings("Howdy partner, sit down! How's it going?", dictionary)
+#   => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+# Please note the order of your keys might be different.
+
+# Quick Tips:
+
+# Recall how to turn strings into arrays and arrays into strings.
+
+def substrings(string, dictionary)
+  array = string.split(" ")
+  hash = dictionary.inject(Hash.new(0)) do |acc, item|
+    array.each do |word|
+      if word.include?(item)
+        acc[item] += 1
+      end
+    end
+    acc
+  end
+  p hash
+end
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
